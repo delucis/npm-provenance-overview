889e128402d578edf2956a0516242ecbd7ed1858
Chris Swithinbank <swithinbank@gmail.com>
2025-10-13 19:59:33 +0200

    Add SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	base: 'npm-provenance-quickview',
+	site: 'https://delucis.github.io/',
 	image: {
 		domains: ['astro.badg.es'],
 	},
